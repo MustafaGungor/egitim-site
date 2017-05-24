4108bb5658b282c5fefcd5cb6a859220a861b287
MustafaGungor <mustafagungormg@outlook.com>
2017-05-24 21:41:52 +0300

    Singleton try

---
+++ egitim-site-web/src/main/java/com/mebitech/samples/quickstart/web/myapp/MainSingleton.java
@@ -0,0 +1,13 @@
+package com.mebitech.samples.quickstart.web.myapp;
+
+/**
+ * Created by Diagram on 24/05/2017.
+ */
+public class MainSingleton {
+    public static void main(String[] args) {
+        final String name = SingletonDesignPattern.getObject().getName("mustafa");
+        String password = SingletonDesignPattern.getObject().getPassword("XcTyHha");
+        System.out.println(name);
+        System.out.println(password);
+    }
+}
---
+++ egitim-site-web/src/main/java/com/mebitech/samples/quickstart/web/myapp/SingletonDesignPattern.java
@@ -0,0 +1,21 @@
+package com.mebitech.samples.quickstart.web.myapp;
+
+/**
+ * Created by Diagram on 24/05/2017.
+ */
+public class SingletonDesignPattern {
+    private static SingletonDesignPattern singletonDesignPattern = new SingletonDesignPattern();
+
+    public SingletonDesignPattern() {
+        System.out.println("singleton yalnız birkez üretiliyor bellekte hafızada tutuluyor");
+    }
+    public static SingletonDesignPattern getObject(){
+        return singletonDesignPattern;
+    }
+    public String getName(String name){
+        return name;
+    }
+    public String getPassword(String password){
+        return password;
+    }
+}
